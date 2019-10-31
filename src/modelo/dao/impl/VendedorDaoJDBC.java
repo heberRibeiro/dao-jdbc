80406d03101651c0ce4f36176e02ad731a80485c
Heber Ribeiro <heber.r@outlook.com>
2019-10-31 14:45:18 -0300

    VendedorDaoJDBC e FabricaDao

---
+++ src/modelo/dao/impl/VendedorDaoJDBC.java
@@ -0,0 +1,42 @@
+package modelo.dao.impl;
+
+import java.util.List;
+
+import modelo.dao.VendedorDao;
+import modelo.entidades.Vendedor;
+
+public class VendedorDaoJDBC implements VendedorDao {
+
+	@Override
+	public void insert(Vendedor dep) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Vendedor dep) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Vendedor findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Vendedor> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
