33ffb308f8bd79ce453f0f0a995d25ccdd22d9f1
Heber Ribeiro <heber.r@outlook.com>
2019-10-31 17:23:20 -0300

    Implementação findById

--- src/modelo/dao/impl/VendedorDaoJDBC.java
+++ src/modelo/dao/impl/VendedorDaoJDBC.java
@@ -1,11 +1,23 @@
 package modelo.dao.impl;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import modelo.dao.VendedorDao;
+import modelo.entidades.Departamento;
 import modelo.entidades.Vendedor;
 
 public class VendedorDaoJDBC implements VendedorDao {
+	
+	private Connection conn = null;
+	
+	public VendedorDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Vendedor dep) {
@@ -27,8 +39,43 @@ public class VendedorDaoJDBC implements VendedorDao {
 
 	@Override
 	public Vendedor findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		java.sql.PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " +
+					"FROM seller INNER JOIN department " +
+					"ON seller.DepartmentId = department.Id " +
+					"WHERE seller.Id = ? ");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if(rs.next()) {
+				Departamento dep = new Departamento();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setNome(rs.getString("DepString"));
+				
+				Vendedor vend = new Vendedor();
+				vend.setId(rs.getInt("Id"));
+				vend.setNome(rs.getString("Name"));
+				vend.setEmail(rs.getString("Email"));
+				vend.setSalarioBase(rs.getDouble("BaseSalary"));
+				vend.setDataAniversario(rs.getDate("BirthDate"));
+				vend.setDepartamento(dep);
+				return vend;
+			}
+			return null;
+			
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatment(st);
+			DB.closeResultSet(rs);
+		}
+		
+		
 	}
 
 	@Override
