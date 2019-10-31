88462a4cce1a6fdfd02471caaa62e494264e4e5d
Heber Ribeiro <heber.r@outlook.com>
2019-10-31 14:01:07 -0300

    Classe Vendedor

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,8 +1,19 @@
 package aplicacao;
 
+import java.util.Date;
+
+import modelo.entidades.Departamento;
+import modelo.entidades.Vendedor;
+
 public class Programa {
 
 	public static void main(String[] args) {
+
+		Departamento dep = new Departamento(1, "Departament de Livros");
+		Vendedor vend = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dep);
+
+		System.out.println(dep);
+		System.out.println(vend);
 
 	}
 
