f48a1835b875cd5871c97159b6771f597ec53a58
Heber Ribeiro <heber.r@outlook.com>
2020-03-18 14:43:38 -0300

    Ajuste classes de implementação JDBC

--- src/modelo/dao/impl/DepartamentoDaoJDBC.java
+++ src/modelo/dao/impl/DepartamentoDaoJDBC.java
@@ -4,10 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.mysql.jdbc.Statement;
 
 import db.DB;
 import db.DbException;
